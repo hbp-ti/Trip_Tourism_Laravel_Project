8f0581cf6fe5b7b09114b4d52f3a6fbad5885d3b
HugoPereira <hbp@ua.pt>
2024-11-13 11:57:43 +0000

    Aded register header and footer

---
+++ TurisGo/resources/views/components/footer.blade.php
@@ -0,0 +1,26 @@
+@vite(['resources/css/footer.css'])
+
+
+<footer class="footer">
+    <div class="footer-section">
+        <h4>About Travel</h4>
+        <p>Discover tips, 
+            destinations, and 
+            inspiration to make 
+            your travel
+            experience
+            unforgettable.</p>
+    </div>
+    <div class="footer-section contact-info">
+        <h4>Contact Information</h4>
+        <a href="mailto:support@turisgo.com">✉️ support@turisgo.com</a>
+        <a href="https://maps.google.com/?q=R.+Cmte.+Pinho+e+Freitas+28,+3750-127+%C3%81gueda" target="_blank">📍 R. Cmte. Pinho e Freitas 28, 3750-127 Águeda</a>
+        <a href="tel:+351910529936">📞 +351 910529936</a>
+    </div>
+    <div class="footer-section estga">
+        <img src="{{ asset('images/estga.png') }}" alt="Logo" class="small-logo">
+          </div>
+    <div class="copyright">
+        Copyright © 2024 TurisGo. All rights reserved.
+    </div>
+</footer>
---
+++ TurisGo/resources/views/components/header.blade.php
@@ -0,0 +1,26 @@
+@vite(['resources/css/header.css'])
+
+<nav class="navbar">
+    <div class="container">
+        <a href="/" class="logo">TurisGo</a>
+
+        <div class="nav-links-container">
+            <ul class="nav-links">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Tours</a></li>
+                <li><a href="">Hotel</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </div>
+
+        <div class="nav-actions">
+            <a href="#" class="language">
+                <img src="{{ asset('images/language_globe.png') }}" alt="Globe" class="language-img" />
+                EN
+            </a>
+            <a href="" class="login-button">Login</a>
+        </div>
+
+    </div>
+</nav>
