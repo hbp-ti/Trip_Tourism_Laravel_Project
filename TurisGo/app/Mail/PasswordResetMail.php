7a20d37fab01e67e2247ce00bedf6e9fd5e1f04b
Hugo Pereira <hbp@ua.pt>
2024-11-21 17:06:40 +0000

    Added reset password logic

--- TurisGo/app/Mail/PasswordResetMail.php
+++ TurisGo/app/Mail/PasswordResetMail.php
@@ -1,5 +1,4 @@
 <?php
-// app/Mail/PasswordResetMail.php
 
 namespace App\Mail;
 
@@ -9,17 +8,14 @@ class PasswordResetMail extends Mailable
 {
     public $resetLink;
 
-    // O construtor recebe o link de redefinição de senha
     public function __construct($resetLink)
     {
         $this->resetLink = $resetLink;
     }
 
-    // Configura o e-mail
     public function build()
     {
-        return $this->subject('Password Reset Link')  // Define o assunto do e-mail
-                    ->view('auth.reset')
-                    ->with(['resetLink' => $this->resetLink]);  // Passa o link para a view
+        return $this->subject('Password Reset Request')
+                    ->view('auth.password_email_reset');
     }
 }
--- TurisGo/routes/web.php
+++ TurisGo/routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\LanguageController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ImageUploadController;
+use Illuminate\Support\Facades\Mail;
 
 
 Route::get('/', function () {
@@ -20,12 +21,20 @@ Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::get('forgot', [AuthController::class, 'showForgotForm'])->name('forgot.form');
     Route::post('forgot', [AuthController::class, 'sendResetLink'])->name('forgot.submit');
-    Route::get('reset/{token}', [AuthController::class, 'showResetForm'])->name('reset.form');
+    Route::get('reset/{token}/{email}', [AuthController::class, 'showResetForm'])->name('reset.form');
     Route::post('reset', [AuthController::class, 'resetPassword'])->name('reset.submit');
     Route::middleware('auth')->group(function () {
         Route::get('profile', [AuthController::class, 'showProfile'])->name('profile');
         Route::get('profile/edit', [AuthController::class, 'editProfile'])->name('profile.edit');
     });
+});
+
+Route::get('/test-email', function () {
+    Mail::raw('Test email body', function ($message) {
+        $message->to('hbp@ua.pt')->subject('Test Email');
+    });
+
+    return 'Test email sent!';
 });
 
 
