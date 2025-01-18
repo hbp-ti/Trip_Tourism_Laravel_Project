c64b78f6095d97a88ee96ce770bd78c551790515
joaofalves <116800132+joaofalves@users.noreply.github.com>
2025-01-18 16:34:55 +0000

    traduções javascript corrigidas

--- TurisGo/resources/js/translations.js
+++ TurisGo/resources/js/translations.js
@@ -1,12 +1,14 @@
 let translations = {};
 
+const loc = document.documentElement.lang; 
+
 const translate = (key) => translations[key] || key;
 
 window.translate = translate;
 
-const loadTranslations = async (locale) => {
+const loadTranslations = async () => {
     try {
-        const response = await fetch(appUrl + `/lang/${locale}.json`);
+        const response = await fetch(appUrl + '/lang/' + loc + '.json');
         if (!response.ok) throw new Error('Falha ao carregar as traduções.');
         return await response.json();
     } catch (error) {
@@ -16,9 +18,10 @@ const loadTranslations = async (locale) => {
 };
 
 const initTranslations = async () => {
-    const urlPath = window.location.pathname.split('/');
-    const currentLocale = urlPath[1];
-    translations = await loadTranslations(currentLocale);
+    //const urlPath = window.location.pathname.split('/');
+    //const currentLocale = urlPath[1];
+    //translations = await loadTranslations(currentLocale);
+    translations = await loadTranslations();
 };
 
 initTranslations();
